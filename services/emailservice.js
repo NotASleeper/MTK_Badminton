9ca80b2f3562425dd7a711a091bb22594dfb0cc4
PhuongHo105 <22521160@gm.uit.edu.vn>
2025-12-26 19:29:40 +0700

    apply singleton design pattern to EmailService

--- services/emailservice.js
+++ services/emailservice.js
@@ -1,28 +1,35 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const sendEmail = async (email, subject, text, html) => {
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.EMAIL_USERNAME, // your email address
-      pass: process.env.EMAIL_PASSWORD, // your email password or app password
-    },
-  });
+class EmailService {
+  constructor() {
+    if (!EmailService.instance) {
+      this.transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_USERNAME, // your email address
+          pass: process.env.EMAIL_PASSWORD, // your email password or app password
+        },
+      });
+      EmailService.instance = this;
+    }
+    return EmailService.instance;
+  }
 
-  let info = await transporter.sendMail({
-    from: '"BadmintonWebsite" <no-reply@badminton.com>', // sender address
-    to: email, // list of receivers
-    subject: subject, // Subject line
-    text: text, // plain text body
-    html: html,
-  });
+  async sendEmail(email, subject, text, html) {
+    return await this.transporter.sendMail({
+      from: '"BadmintonWebsite" <no-reply@badminton.com>', // sender address
+      to: email, // list of receivers
+      subject: subject, // Subject line
+      text: text, // plain text body
+      html: html,
+    });
+  }
+}
 
-  return info;
-};
-
+const emailService = new EmailService();
 module.exports = {
-  sendEmail,
+  sendEmail: emailService.sendEmail.bind(emailService),
 };
